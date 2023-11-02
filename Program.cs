edd88664ea25d45badbaee347aa523eb8bf1e1d8
Alican Dağıdır <alicandagidir@hotmail.com>
2023-11-02 17:07:59 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentClassPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool control = true;
+            Student student1 = new Student(52, "Alican", "Dağıdır", 75, 95, 80, "Dgdr University");
+
+            Console.WriteLine("Enter the given number between 1-4 to get the given information please");
+            
+            
+
+            while (control)
+            {
+                ShowProcedures();
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        student1.ShowStudentInfo();
+                        break;
+                    case "2":
+                        double StudentAvg = student1.ShowStudentAvg();
+                        Console.WriteLine("Student's avg is : " + StudentAvg);
+                        break;
+                    case "3":
+                        student1.ShowSchoolName();
+                        break;
+                    case "4":
+                        control = false;
+                        break;
+                }
+            }
+            
+        }
+        static void ShowProcedures()
+        {
+            Console.WriteLine("1 - Show Student Info");
+            Console.WriteLine("2 - Show Students Average");
+            Console.WriteLine("3 - Show Student's School");
+            Console.WriteLine("4 - Log out from the system");
+            Console.WriteLine("");
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Console = System.Console;
+
+namespace StudentClassPractice
+{
+    public class Student
+    {
+        private int StudentNumber;
+        private string StudentName;
+        private string StudentSurname;
+        private int midterm1;
+        private int midterm2;
+        private int final;
+        private string SchoolName;
+
+
+        public Student(int _studentNumber, string _studentName, string _studentSurname, int _midterm1, int _midterm2, int _final, string _schoolName)
+        {
+                StudentNumber = _studentNumber;
+                StudentName = _studentName;
+                StudentSurname = _studentSurname;
+                midterm1 = _midterm1;
+                midterm2 = _midterm2;
+                final = _final;
+                SchoolName = _schoolName;
+        }
+        public void ShowStudentInfo()
+        {
+            Console.WriteLine("Student name is : "+StudentName);
+            Console.WriteLine("Student surname is : " + StudentSurname);
+            Console.WriteLine("Student number is : " +StudentNumber);
+        }
+
+        public double ShowStudentAvg()
+        {
+            double ortalama = midterm1 * 0.2 + midterm2 * 0.2 + final * 0.6;
+
+            return ortalama;
+        }
+
+        public void ShowSchoolName()
+        {
+            Console.WriteLine("Student's Schoolname is : "+SchoolName);
+        }
+    }
+}
